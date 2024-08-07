67c6cfb8782387d52259db99c3165e609f1ec28f
marcosxz <marcosvioliviera7@gmail.com>
2024-08-07 11:32:46 -0300

    test

--- gerador_de_senha/src/App.js
+++ gerador_de_senha/src/App.js
@@ -1,37 +1,37 @@
-import { useState } from "react";
-import './styles.css';
+import { useState } from "react"
+import './styles.css'
 
 function App() {
-  const [password, setPassword] = useState("");
-  const [copyText, setCopyText] = useState("Copiar");
-
+  const [password, setPassword] = useState("")
+  const [copyText, setCopyText] = useState("Copiar")
   function generate() {
-    const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?";
-    const length = 12;
-    let newPassword = "";
 
-    for (let i = 0; i < length; i++) {
-      const position = Math.floor(Math.random() * characters.length);
-      newPassword += characters[position];
+    const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
+    const length = 12
+    let newPassword = ""
+
+     for (let i = 0; i < length; i++) {
+       const position = Math.floor(Math.random() * characters.length)
+       newPassword += characters[position]
     }
 
-    setPassword(newPassword);
-    setCopyText("Copiar");
+     setPassword(newPassword)
+    setCopyText("Copiar")
   }
 
-  function copyToClipboard() {
-    window.navigator.clipboard.writeText(password);
-    setCopyText("Copiado!");
+   function copyToClipboard() {
+     window.navigator.clipboard.writeText(password)
+     setCopyText("Copiado!")
   }
 
-  return (
+   return (
     <div className="App">
       <h1>Gerador de senhas</h1>
-      <button className="generate-button" onClick={generate}>Gerar!</button>
-      <button className="copy-button" onClick={copyToClipboard}>{copyText}</button>
+       <button onClick={generate}>Gerar!</button>
+       <button onClick={copyToClipboard}>{copyText}</button>
+      
       <div className="password">{password}</div>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
